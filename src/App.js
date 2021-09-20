0d1f03c98bc89543eacd08047521ad8524b59914
JensRosseel <slaythehorizon.games@gmail.com>
2021-09-20 15:59:30 +0200

    Add pages and navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from './Home'
+import Projects from './Projects'
+import Contact from './Contact'
+
+class Nav extends React.Component{
+  render(){
+    return(
+      <div className="navWindow">
+        <Link to="/">Home</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/contact">Contact Me</Link>
+      </div>
+    )
+  }
+}
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/projects">
+          <Nav />
+          <Projects />
+        </Route>
+        <Route path="/contact">
+          <Nav />
+          <Contact />
+        </Route>
+        <Route path="/">
+          <Nav />
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="container">
+      <div className="content">
+        <p>Hi, I'm Jens Rosseel<br></br>I'm a full-stack developer</p>
+        <button>View my projects &#x21e8;</button>
+      </div>
+    </div>
+  )
+}
