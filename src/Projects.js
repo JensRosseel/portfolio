f52de2c5d5a01198be6b09ea27375769b4e66123
JensRosseel <slaythehorizon.games@gmail.com>
2021-09-23 14:35:01 +0200

    Add project to projects page

--- src/Projects.js
+++ src/Projects.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import './projects.css'
+import spaceinvaders from './img/spaceinvaders.JPG'
 
 export default function Projects() {
     return (
         <div className="container">
             <div className="card">
-                <img src="" alt="project" />
-                <div className="title">title</div>
-                <div className="button"><Link to="">Go to project</Link></div>
+                <img src={ spaceinvaders } alt="space-invaders" />
+                <div className="title">Space Invaders</div>
+                <div className="button"><a href="https://spaceinvaders-jensrosseel.netlify.app">Go to project</a></div>
             </div>
             <div className="card">
                 <img src="" alt="project" />
                 <div className="title">title</div>
-                <div className="button"><Link to="">Go to project</Link></div>
+                <div className="button"><a href="/projects">Go to project</a></div>
             </div>
             <div className="card">
                 <img src="" alt="project" />
                 <div className="title">title</div>
-                <div className="button"><Link to="">Go to project</Link></div>
+                <div className="button"><a href="/projects">Go to project</a></div>
             </div>
         </div>
     )
