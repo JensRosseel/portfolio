80d2a439cad0825eaf88c41013ec37afe23f5d15
Jens Rosseel <slaythehorizon.games@gmail.com>
2022-01-05 03:03:30 +0100

    Change theme

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,24 +2,24 @@ import React from 'react'
 import * as AiIcons from 'react-icons/ai'
 import './Contact.css'
 import { IconContext } from 'react-icons'
+import jester from '../img/jester.png'
 
 export default function Contact() {
     return (
         <>
-            <IconContext.Provider value={{color: 'black'}}>
+            <IconContext.Provider value={{color: 'white'}}>
                 <div className="contact-container">
                     <div className="contact">
                         <p>Contact me at</p>
                         <div className="media-links">
                             <p>
                                 E-mail: <a href="mailto:jensrosseel.business@gmail.com">jensrosseel.business@gmail.com</a><br/>
-                                GitHub: <a href="https://github.com/JensRosseel" target="_blank" rel="noopener noreferrer">https://github.com/JensRosseel</a>
                             </p>
                         </div>
                         <div className="media-buttons">
                             <a href="mailto:jensrosseel.business@gmail.com"><AiIcons.AiOutlineMail /></a>
-                            <a href="https://github.com/JensRosseel" target="_blank" rel="noopener noreferrer"><AiIcons.AiFillGithub /></a>
                         </div>
+                        <img src={jester} alt='jester' />
                     </div>
                 </div>
             </IconContext.Provider>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Home.css'
+import jester from '../img/jester.png'
 
 export default function Home() {
   return (
     <>
       <div className="home-container">
         <div className="home-content">
-          <p>Hi, I'm Jens Rosseel<br></br>I'm a Web Developer</p><br/>
-          <Link to="/projects" className='home-button'>View my projects &#x21e8;</Link>
+          <p>Hi, I'm Jens Rosseel<br></br>I'm a Web Developer from Belgium with a passion for &lt;code/&gt;</p><br/>
+          <Link to="/projects" className='home-button'>View my projects &#x21e8;</Link><br />
+          <img src={jester} alt='jester' />
         </div>
       </div>
     </>
